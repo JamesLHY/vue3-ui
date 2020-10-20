a057f78227def70ef6f4a69c8a292037066d317b
JamesLHY <84779512@qq.com>
2020-10-20 09:57:27 +0800

    高亮路由

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,112 +1,129 @@
 <template>
-    <div class="layout">
-        <TopNav :toggleMenuButtonVisible="true" class="nav" />
-        <div class="content">
-            <aside v-if="asideVisible">
-                <h2>文档</h2>
-                <ol>
-                    <li>
-                        <router-link to="/Doc/Intro">介绍</router-link>
-                    </li>
-                    <li>
-                        <router-link to="/Doc/Install">安装</router-link>
-                    </li>
-                    <li>
-                        <router-link to="/Doc/GetStarted">开始使用</router-link>
-                    </li>
-                </ol>
-                <h2>组件列表</h2>
-                <ol>
-                    <li>
-                        <router-link to="/Doc/Switch">Switch组件</router-link>
-                    </li>
-                    <li>
-                        <router-link to="/Doc/Button">Button组件</router-link>
-                    </li>
-                    <li>
-                        <router-link to="/Doc/DiaLog">Dialog组件</router-link>
-                    </li>
-                    <li>
-                        <router-link to="/Doc/Tabs">Tabs组件</router-link>
-                    </li>
-                </ol>
-            </aside>
-            <main>
-                <router-view/>
-            </main>
-        </div>
+  <div class="layout">
+    <TopNav :toggleMenuButtonVisible="true" class="nav" />
+    <div class="content">
+      <aside v-if="asideVisible">
+        <h2>文档</h2>
+        <ol>
+          <router-link to="/Doc/Intro">
+            <li>介绍</li>
+          </router-link>
+          <router-link to="/Doc/Install">
+            <li>安装</li>
+          </router-link>
+          <router-link to="/Doc/GetStarted">
+            <li>开始使用</li>
+          </router-link>
+        </ol>
+        <h2>组件列表</h2>
+        <ol>
+          <router-link to="/Doc/Switch">
+            <li>Switch组件</li>
+          </router-link>
+          <router-link to="/Doc/Button">
+            <li>Button组件</li>
+          </router-link>
+          <router-link to="/Doc/DiaLog">
+            <li>Dialog组件</li>
+          </router-link>
+          <router-link to="/Doc/Tabs">
+            <li>Tabs组件</li>
+          </router-link>
+        </ol>
+      </aside>
+      <main>
+        <router-view />
+      </main>
     </div>
+  </div>
 </template>
 <script lang="ts">
-    import TopNav from '../components/TopNav.vue'
-    import {inject, Ref} from "vue";
+import TopNav from "../components/TopNav.vue";
+import { inject, Ref } from "vue";
 
-    export default {
-        components: {
-            TopNav
-        },
-        setup() {
-            const asideVisible = inject<Ref<boolean>>('asideVisible')
-            return {asideVisible}
-        }
-    }
+export default {
+  components: {
+    TopNav,
+  },
+  setup() {
+    const asideVisible = inject<Ref<boolean>>("asideVisible");
+    return { asideVisible };
+  },
+};
 </script>
 <style lang="scss" scoped>
-    .layout {
-        display: flex;
-        flex-direction: column;
-        height: 100vh;
+.router-link-active {
+  position: relative;
+  color: #1890ff;
+}
+.router-link-active::before {
+  z-index: -1;
+  position: fixed;
+  display: block;
+  clear: both;
+  content: "";
+  width: 100%;
+  height: 2em;
+  background: #e6f7ff;
+  transform: translateY(-0.5em);
+}
 
-        > .nav {
-            flex-shrink: 0;
-        }
+.layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
 
-        > .content {
-            flex-grow: 1;
-            padding-top: 60px;
-            padding-left: 156px;
-            @media (max-width: 500px) {
-                padding-left: 0;
-            }
-        }
+  > .nav {
+    flex-shrink: 0;
+  }
+
+  > .content {
+    flex-grow: 1;
+    padding-top: 60px;
+    padding-left: 156px;
+    @media (max-width: 500px) {
+      padding-left: 0;
     }
+  }
+}
 
-    .content {
-        display: flex;
+.content {
+  display: flex;
 
-        > aside {
-            flex-shrink: 0;
-        }
+  > aside {
+    flex-shrink: 0;
+  }
 
-        > main {
-            flex-grow: 1;
-            padding: 16px;
-            background: lightgreen;
-        }
+  > main {
+    z-index: 10;
+    flex-grow: 1;
+    padding: 16px;
+    background: lightgreen;
+  }
+}
+
+aside {
+  width: 150px;
+  padding: 16px 0;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding-top: 70px;
+  height: 100%;
+
+  > h2 {
+    margin-bottom: 4px;
+  }
+
+  > ol {
+    a
+     > li {
+      margin: 10px 0;
     }
+  }
 
-    aside {
-        background: lightblue;
-        width: 150px;
-        padding: 16px;
-        position: fixed;
-        top: 0;
-        left: 0;
-        padding-top: 70px;
-        height: 100%;
-
-        > h2 {
-            margin-bottom: 4px;
-        }
-
-        > ol {
-            > li {
-                padding: 4px 0;
-            }
-        }
-
-        main {
-            overflow: auto;
-        }
-    }
+  main {
+    overflow: auto;
+  }
+}
 </style>
