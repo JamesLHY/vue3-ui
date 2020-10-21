ee61121987e2b80175eec17a1f3a8712bb7f7b4e
JamesLHY <84779512@qq.com>
2020-10-21 16:57:12 +0800

    pdate

--- src/views/Install.vue
+++ src/views/Install.vue
@@ -2,9 +2,9 @@
   <article class="markdown-body">
     <h1>安装</h1>
     <p>打开终端运行下列命令：</p>
-    <pre><code>npm install king-ui</code></pre>
+    <pre><code>npm install vue3-pure-wheel</code></pre>
     <p>或</p>
-    <pre><code>yarn add king-ui</code></pre>
+    <pre><code>yarn add vue3-pure-wheel</code></pre>
 
     <p>
       下一节：
